f808df69abd063bf038c08ce977da387bb3d10b4
user <user@use1r-PC>
2017-02-06 10:07:47 +0800

    new demos

---
+++ Architect/src/architect/main/NonStaticMain.java
@@ -0,0 +1,7 @@
+package architect.main;
+
+public class NonStaticMain {
+	public void main(String[] args) {// This method won't execute.
+		System.out.println("Hello World");
+	}
+}
---
+++ Architect/src/architect/main/OrderAlteredMain.java
@@ -0,0 +1,7 @@
+package architect.main;
+
+public class OrderAlteredMain {
+	static public void main(String[] args) {// It is okay to switch the order of static and public.
+		System.out.println("Hello World");
+	}
+}
